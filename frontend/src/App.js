4608dbd6f75adc0e9ddce7c7a27889d3537db27e
DiegoVelazqz <diegoa1962@hotmail.com>
2023-06-13 13:35:58 -0600

    chatbot with chatgpt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,10 +12,98 @@ import GrandPrixPage from './pages/GrandPrixPage';
 import DriversPage from './pages/DriversPage';
 import TeamsPage from './pages/TeamsPage';
 import HistoricalDataPage from './pages/HistoricalDataPage';
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator
+} from "@chatscope/chat-ui-kit-react";
 
+
+const API_KEY = "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";
 function App() {
+  const [typing, setTyping] = React.useState(false);
+
+  const [messages, setMessages] = React.useState([
+    {
+      message: "Hello",
+      sender: "ChatGPT"
+    }
+  ])
+
+  const handleSend = async(message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    }
+    //new array of messages
+    const newMessages = [...messages, newMessage]; //all the old messages + new message
+
+    //update messages state
+    setMessages(newMessages);
+
+    //set isTyping to true
+    setTyping(true);
+
+    //process message to chatgpt
+    await processMessageToChatGPT(newMessages);
+  }
+  async function processMessageToChatGPT(chatMessages){
+    //chatMessages{ sender: "user" or "chat", message: "message"}
+    //apiMessages{ role: "user" or "assistant", content: "message"}
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if(messageObject.sender === "ChatGPT"){
+        role = "assistant";
+      } else{
+        role = "user";
+      }
+      return { role: role, content: messageObject.message }
+    });
+
+    //role: "user" -> a message from the user, "assistant" -> a response from chatGPT
+    //"system" -> a system message from chatGPT "
+    const systemMessage = {
+      role : "system",
+      content: "Explain all concepts like I'm 5 years old" // Speak like a pirate
+
+    }
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages" : [ 
+        systemMessage,
+        ...apiMessages,//[message 1, message 2, message 3]
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions",{
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      console.log(data.choices[0].message.content)
+      setMessages(
+        [...chatMessages, {
+        message: data.choices[0].message.content,
+        sender: "ChatGPT"
+        }]);
+      setTyping(false);
+    });
+  }
+
   return (
-    <Router>
+    /*<Router>
       <MainNavbar />
       <SecondaryNavbar />
       <Routes>
@@ -28,8 +116,31 @@ function App() {
         <Route path="/drivers" component={DriversPage} />
         <Route path="/teams" component={TeamsPage} />
         <Route path="/historical-data" component={HistoricalDataPage} />
-      </Routes>
-    </Router>
+      </Routes>*/
+      
+      <div className="App">
+        <div style={{position: "relative", height:"800px",width:"700px"}}>
+          <MainContainer>
+            <ChatContainer>
+              <MessageList
+                typingIndicator = { typing ? <TypingIndicator content="ChatGPT is typing"/>: null }
+              >
+                
+                {messages.map((message, i) => {
+                  return <Message key={i} model={message} />
+                })}
+
+              </MessageList> 
+              <MessageInput placeholder="Type message here" onSend={handleSend}/>
+             
+            </ChatContainer>
+          </MainContainer>  
+
+        </div>
+        
+      </div>
+    //</Router>
+    
   );
 }
 
--- frontend/src/pages/RulesPage.js
+++ frontend/src/pages/RulesPage.js
@@ -1,8 +1,19 @@
 // src/pages/RulesPage.js
-import React from 'react';
+import React, { useState } from 'react';
 
 const RulesPage = () => {
-  return <div>Welcome to the Home Page</div>;
+  const [message, setMessage] = useState('Welcome to the rules');
+
+  const handleUpdateMessage = () => {
+    setMessage('New message');
+  };
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <button onClick={handleUpdateMessage}>Update Message</button>
+    </div>
+  );
 };
 
 export default RulesPage;
