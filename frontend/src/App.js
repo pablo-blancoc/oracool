6e5dd3ab4ba049a9dad96f726ab63efa9744b261
Jaime <a01638037@itesm.mx>
2023-06-13 08:41:13 -0600

    Router syntax correction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainNavbar from './navigation/MainNavbar';
 import SecondaryNavbar from './navigation/SecondaryNavbar';
 import HomePage from './pages/HomePage';
@@ -13,24 +13,44 @@ import DriversPage from './pages/DriversPage';
 import TeamsPage from './pages/TeamsPage';
 import HistoricalDataPage from './pages/HistoricalDataPage';
 
-function App() {
+// function App() {
+//   return (
+//     <Router>
+//       <MainNavbar />
+//       <SecondaryNavbar />
+//       <Routes>
+//         <Route path="/" exact element={HomePage} />
+//         <Route path="/signin" element={SignInPage} />
+//         <Route path="/rules" element={RulesPage} />
+//         <Route path="/simulation" element={SimulationPage} />
+//         <Route path="/leaderboard" element={LeaderboardPage} />
+//         <Route path="/grandprix" element={GrandPrixPage} />
+//         <Route path="/drivers" element={DriversPage} />
+//         <Route path="/teams" element={TeamsPage} />
+//         <Route path="/historical-data" element={HistoricalDataPage} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
   return (
     <Router>
       <MainNavbar />
       <SecondaryNavbar />
       <Routes>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/signin" component={SignInPage} />
-        <Route path="/rules" component={RulesPage} />
-        <Route path="/simulation" component={SimulationPage} />
-        <Route path="/leaderboard" component={LeaderboardPage} />
-        <Route path="/grandprix" component={GrandPrixPage} />
-        <Route path="/drivers" component={DriversPage} />
-        <Route path="/teams" component={TeamsPage} />
-        <Route path="/historical-data" component={HistoricalDataPage} />
+        <Route path="/" exact element={<HomePage/>} />
+        <Route path="/signin" element={<SignInPage/>} />
+        <Route path="/rules" element={<RulesPage/>} />
+        <Route path="/simulation" element={<SimulationPage/>} />
+        <Route path="/leaderboard" element={<LeaderboardPage/>} />
+        <Route path="/grandprix" element={<GrandPrixPage/>} />
+        <Route path="/drivers" element={<DriversPage/>} />
+        <Route path="/teams" element={<TeamsPage/>} />
+        <Route path="/historical-data" element={<HistoricalDataPage/>} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
