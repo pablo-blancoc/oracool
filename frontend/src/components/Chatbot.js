02196c40fc9cf0b3b19d463c869becbcfad3951b
DiegoVelazqz <diegoa1962@hotmail.com>
2023-06-14 12:33:00 -0600

    fixed

--- frontend/src/components/Chatbot.js
+++ frontend/src/components/Chatbot.js
@@ -11,7 +11,7 @@ import {
 
 
 
-// setear:const API_KEY = "dasdasd";
+const API_KEY = "dasdasd";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
