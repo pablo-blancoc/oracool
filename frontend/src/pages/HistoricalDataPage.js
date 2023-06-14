ca14b8db4380a271f801de82cde309ac4fa263c0
Jesus Palomino <73486611+Jesus-Palomino@users.noreply.github.com>
2023-06-13 18:22:23 -0600

    Rules page complete

--- frontend/src/pages/HistoricalDataPage.js
+++ frontend/src/pages/HistoricalDataPage.js
@@ -2,7 +2,14 @@
 import React from 'react';
 
 const HistoricalDataPage = () => {
-  return <div>Welcome to the Home Page</div>;
+  return (
+    <>
+    <div>Welcome to the Home Page</div>;
+    <iframe src="https://www.formula1.com" title="F1 Website" width="100%" height="600"></iframe>
+    </>
+    
+  )
+  
 };
 
 export default HistoricalDataPage;
